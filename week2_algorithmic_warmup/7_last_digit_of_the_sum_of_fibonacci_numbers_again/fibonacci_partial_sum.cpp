a1d94ad158a2ee87a90ecc4840dc55cb13700f7b
William Griffin <w.griffin13@gmail.com>
2020-06-29 16:32:35 -0700

    sum of squares last digit

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -21,8 +21,27 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+int fibonacci_last_digit(long long n) {
+
+    long long current   = 0;
+    long long next      = 1;
+    long long old_next  = 0;
+    for (long long i = 0; i < n; ++i) {
+        old_next = next;
+        next = (current + next) % 10;
+        current = old_next;
+    }
+    return current;
+}
+
+int partial_sum_fibonacci_last_digit(long long from, long long to) {
+    int end_num = (fibonacci_last_digit((to + 2) % 60) + 9) % 10;
+    int start_num = (fibonacci_last_digit((from + 1) % 60) + 9) % 10;
+    return (end_num - start_num + 10) % 10;
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << partial_sum_fibonacci_last_digit(from, to) << '\n';
 }
--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -18,8 +18,27 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+int fibonacci_last_digit(long long n) {
+
+    long long current   = 0;
+    long long next      = 1;
+    long long old_next  = 0;
+    for (long long i = 0; i < n; ++i) {
+        old_next = next;
+        next = (current + next) % 10;
+        current = old_next;
+    }
+    return current;
+}
+
+int sum_squares_fibonacci_last_digit(long long n) {
+    int vertical_side = fibonacci_last_digit((n) % 60);
+    int horizontal_side = fibonacci_last_digit((n + 1) % 60);
+    return (vertical_side * horizontal_side) % 10;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << sum_squares_fibonacci_last_digit(n);
 }
